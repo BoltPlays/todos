a0cee6c7437630918b72b00fdbbe5d1fa192ab39
archie <archienewman09@gmail.com>
2025-08-06 19:49:28 +0100

    todos endpoint

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import Fastify from 'fastify';
+import MongoDb from '@fastify/mongodb';
 
 import getTodosRoute from './routes/todos/get.js';
 
@@ -9,6 +10,14 @@ const fastify = Fastify({
 const PORT: number = 3000;
 
 fastify.register(getTodosRoute, { prefix: '/api/todos' });
+
+fastify.register(MongoDb, {
+  // force to close the mongodb connection when app stopped
+  // the default value is false
+  forceClose: true,
+
+  url: 'mongodb://admin:admin@localhost:27017/todos?authSource=admin'
+})
 
 // Run the server!
 fastify.listen({ port: PORT }, function (err, _address) {
